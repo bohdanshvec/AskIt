c5a85495e77642d6243ff8a5cebe53db88ade75d
bohdanshvetc <dan6147@ukr.net>
2023-10-08 17:51:36 +0300

    lesson_3

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@ class Question < ApplicationRecord
   validates :title, presence: true, length: {minimum: 2}
   validates :body, presence: true, length: {minimum: 2}
 
+  scope :ordered, -> { order(id: :desc) }
+
   def formatted_created_at
     created_at.strftime("%Y-%m-%d %H:%M:%S")
   end
